538199a67836459fe6c16adb430b5c8a42f75bd6
Creeford Sanchez <creewrk@gmail.com>
2024-10-26 15:51:15 -0700

    first commit

--- source/js/index.js
+++ source/js/index.js
@@ -3,10 +3,25 @@
 function selector(selector) {
   return document.querySelector(selector);
 }
-
+function selectorAll(selector) {
+  return document.querySelectorAll(selector);
+}
 function listener(selector, event, callback) {
   return selector.addEventListener(event, callback);
 }
+
+const heroSlider = selector('.hero-slider');
+const heroSliderBtn = [...selector('.hero-bubble-box').children];
+
+heroSliderBtn.forEach((button, index) => {
+  listener(button, 'click', () => {
+    const parent = [...heroSlider.children];
+    let target = parent[index].getBoundingClientRect();
+    heroSlider.scrollLeft += target.x;
+    console.log("test");
+  });
+});
+
 const navBtn = selector('.fa-bars');
 const navContent = selector('.header-menu');
 listener(navBtn, 'click', () => {
@@ -43,9 +58,19 @@ function scrollFunction(rsBtn, lfBtn, boxSlider) {
     if(index < 0) {
       index++;
     }
+    //gain access to the possition value
     let child = array[index].getBoundingClientRect();
     boxSlider
     .scrollLeft += child.x;
-    
   });
 }
+
+const expandBtn = Array.from(selectorAll('.page-title'));
+const contents = Array.from(selectorAll('.contents'));
+const angleIcon =  Array.from(selectorAll('.fa-angle-down'));
+expandBtn.forEach((button, index) => {
+  listener(button, 'click', () => {
+    contents[index].classList.toggle('content-transition');
+    angleIcon[index].classList.toggle('.angle-rotate');
+  })
+})
